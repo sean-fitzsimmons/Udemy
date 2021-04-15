14c61bc8ccd90f896234cd15ca2ee42ead14c471
Sean Fitzsimmons <seanfitzsimmons12@gmail.com>
2021-04-14 22:47:35 -0700

    Add color to winner/loser score

--- score.keep.js
+++ score.keep.js
@@ -8,7 +8,7 @@ const winningScoreSelect = document.querySelector('#playTo')
 
 let p1Score = 0; 
 let p2Score = 0; 
-let winningScore = 5; 
+let winningScore = 3; 
 let isGameOver = false; 
 
 p1Button.addEventListener('click', function () {
@@ -16,6 +16,9 @@ p1Button.addEventListener('click', function () {
     p1Score += 1; 
     if (p1Score === winningScore) {
         isGameOver = true; 
+        p1Display.classList.add('winner'); 
+        p2Display.classList.add('loser'); 
+      
     }
     p1Display.textContent = p1Score;
     }
@@ -26,6 +29,9 @@ p2Button.addEventListener('click', function () {
     p2Score += 1; 
     if (p2Score === winningScore) {
         isGameOver = true; 
+        p2Display.classList.add('winner'); 
+        p1Display.classList.add('loser'); 
+        
     }
     p2Display.textContent = p2Score;
     }
@@ -46,4 +52,7 @@ function reset () {
     p2Score = 0; 
     p1Display.textContent = 0; 
     p2Display.textContent = 0; 
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+
 }
