66d3228eb0222d7fd4578458621b9ea9212469d3
Sean Fitzsimmons <seanfitzsimmons12@gmail.com>
2021-04-30 10:19:54 -0700

    section 28, api 's and making search tools

---
+++ section.28.js
@@ -0,0 +1,108 @@
+// const req = new XMLHttpRequest(); 
+
+// req.onload = function () { 
+//     console.log("all done with request");
+//    const data = JSON.parse(this.responseText);
+//    console.log(data.ticker.price)
+    
+// }
+// req.onerror = function () {
+//     console.log("error!")
+//     console.log(this); 
+// }
+
+// req.open('GET', 'https://api.cryptonator.com/api/full/btc-usd')
+// req.send(); 
+
+// XMLHttpRequest, its a whole lot of code for a little output. 
+
+// fetch("https://api.cryptonator.com/api/full/btc-usd")
+// .then(res => {
+//     console.log("RESPONSE", res)
+//     return res.json()
+// })
+// .then(data => {
+//     console.log("data parsed...", data)
+//     console.log(data.ticker.price)
+// })
+// .catch(e => {
+//     console.log("Oh no! Error!", e)
+// })
+
+// This is the newer and easier way to get api data 
+// Promise version 
+
+// const fetchBitcoinPrice = async () => {
+//     try {
+//    const res = await fetch('https://api.cryptonator.com/api/full/btc-usd'); 
+//    const data = await res.json();
+//    console.log(data.ticker.price); 
+// } catch (e) {
+//     console.log("something went wrong!", e)
+
+// }
+// }
+//  The much cleaner async method of getting api's 
+// the try and catch will grab anything that goes wrong 
+
+// axios.get("https://api.cryptonator.com/api/full/btc-usd")
+// .then(res => {
+//     console.log(res.data.ticker.price) 
+// })
+// .catch(err => {
+//     console.log("error!", err)
+// })
+
+// This is the absolute best way to get info via api request, super simple and clean 
+
+// const fetchBitcoinPrice = async () => {
+// try {
+//  const res = await axios.get("https://api.cryptonator.com/api/full/btc-usd")
+// console.log(res.data.ticker.price)
+// } catch(e) {
+// }
+// }
+
+// // This is the try catch method 
+// const jokes = document.querySelector('#jokes'); 
+// const button = document.querySelector('button'); 
+
+
+// const addNewJoke = async () => {
+//    const jokeText = await getDadJoke()
+//     const newLi = document.createElement('LI'); 
+//     newLi.append(jokeText)
+//     jokes.append(newLi)
+
+// }
+// const getDadJoke = async () => {
+//     try {
+//     const config = { headers: { Accept: 'application/json'}}
+//     const res = await axios.get('https://icanhazdadjoke.com/?', config)
+//     return res.data.joke; 
+//     } catch(e) {
+//         return "No jokes availible"
+//     } 
+// }
+// button.addEventListener('click', addNewJoke)
+
+const form = document.querySelector('#searchForm'); 
+form.addEventListener('submit', async function (e) {
+    e.preventDefault(); 
+    const searchTerm = form.elements.query.value; 
+    const config = { params: { q: searchTerm} }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+    form.elements.query.value = ''; 
+
+})
+
+const makeImages = (shows) => {
+    for(let result of shows) {
+        if(result.show.image) {
+        const img = document.createElement('IMG'); 
+    img.src = result.show.image.medium;  
+    document.body.append(img)
+    }
+}
+}
--- section.28.md
+++ section.28.md
@@ -42,4 +42,33 @@
     - body 
       - the core of the info, could be html, could be json could be something else 
   ## query strings and headers 
+  - on normal site you can normally find the api in the bottom of the page or in the developers section if it has it
+  - ?q=:query, this is a variable and something you have to provide for the api  
+    - a lot fo api's require the use of query strings 
+      - query strings are information we can provide to any url 
+    - some need a header 
+        - go under the header tab 
+        - key = Accept and value = application/json 
+          - This is going to pull up the jason formate for the api 
+  ## Making XHR's
+  -XMLHttpRequest 
+    - The original way of sending request via js 
+    - absolutely terrible 
+  ## The fetch api 
+  - Newer way to write api request in javascprit 
+  - Just much simpler way to get api requests 
+  - have to use try and catch to get mistakes
+  ## Intro to Axios 
+  - This is a library built on top of fetch that gives a handful of methods to write simple requests 
+  - External script to throw into the HTML with 
+    - <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> 
+  - Axios makes it so we do not have to do a return promise, it merges the promises into one 
+  - You can use axios in the style sheet and on a server, so people like that about it 
+  - this is also useful for making a bunch of different types of requests as well 
+## setting headers with axios 
+- we can specifiy headers 
+- can attach the data to a button and have real time api rocking in the dom 
+- do not do an infitinte loop because api's can ban you 
   
+## Tv show search app 
+- 
